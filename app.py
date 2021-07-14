5f40402428b72ccb2bbdb1a36ca78bcb7d4cc4f3
fdquinones <fdquinones@gmail.com>
2021-07-14 17:59:03 -0500

    cambios para presentar lo que se este transmitiendo

--- app.py
+++ app.py
@@ -93,6 +93,25 @@ def validate_param(param_name):
     print("[INFO] PARAM_NAME:", param_name, "PARAM_VALUE: ", os.environ[param_name])
     return os.environ[param_name]
 
+def gen_frames():  # generate frame by frame from camera
+    while True:
+        # Capture frame-by-frame
+        img = chrys.VideoLatestImage()
+        if img is  None:
+            break
+        else
+            frame = img.data
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+                   
+                   
+@app.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    
 @app.route('/')
 def index():
     """Video streaming home page."""
