23b9b19292957e39b7ddb8b16215ca528530f663
fdquinones <fdquinones@gmail.com>
2021-07-15 15:30:50 -0500

    cambios

--- static/js/main.js
+++ static/js/main.js
@@ -20,6 +20,36 @@
 		$(table2).find("."+column).removeClass('hov-column-'+ verTable);
 		$(table1).find(".row100.head ."+column).removeClass('hov-column-head-'+ verTable);
 	});
+	
+	// Your web app's Firebase configuration
+	var firebaseConfig = {
+		apiKey: "AIzaSyBNXxTi201phes4B24moqj_bUIAZvo8cbw",
+		authDomain: "utplcovid.firebaseapp.com",
+		projectId: "utplcovid",
+		storageBucket: "utplcovid.appspot.com",
+		messagingSenderId: "1052404064289",
+		appId: "1:1052404064289:web:385ce48af2131cc38ec360"
+	};
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+
+	var article_id = 1;
+	var article = {
+		'fecha': firebase.firestore.FieldValue.serverTimestamp(),
+		'cpu': 50,
+		'imagen': 'hola.jpg',
+	}
+
+	firebase.database().ref('facemask/' + article_id).set(article);
+	
+	var eventsRef = firebase.database().ref('facemask/');
+		eventsRef.on('child_added', (data) => {
+			console.log("llegando evento en portal de utpl");
+			console.log(data);
+		//addCommentElement(postElement, data.key, data.val().text, data.val().author);
+	});
+
+
     
 
 })(jQuery);
